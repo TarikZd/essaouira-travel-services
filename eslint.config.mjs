5f2bf52a2332780ce5c0038bb483c9fbe9c3c07a
Ziad Tarik <ziad2014.TT@gmail.com>
2025-10-20 22:04:58 +0000

    Fix build and deployment errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,7 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
-  },
-];
+/** @type {import('eslint').Linter.Config} */
+const eslintConfig = {
+  extends: ["next/core-web-vitals", "next/typescript"],
+};
 
 export default eslintConfig;
--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,8 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['images.unsplash.com', 'picsum.photos'],
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.optimization.splitChunks.cacheGroups = {
-        framework: {
-          chunks: 'all',
-          name: 'website',
-          test: /(?<!node_modules.*)[\\/]node_modules[\\/](react|react-dom|@next|scheduler|prop-types|use-subscription)[\\/]/,
-          priority: 40,
-          enforce: true,
-        },
-      };
-    }
-    return config;
-  },
+    images: {
+        domains: ['images.unsplash.com', 'picsum.photos'],
+    },
 };
 
 export default nextConfig;
