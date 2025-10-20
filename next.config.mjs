0c176d8f38d601ac2fac06f7f8111ea3e9ad8e20
Tarik Ziad <ziad2014.tt@gmail.com>
2025-10-20 17:50:05 +0100

    Optimize Next.js build for Cloudflare Pages deployment
    - Add webpack optimizations to reduce bundle size
    - Enable CSS and SWC minification
    - Split chunks to avoid file size limit

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,24 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com', 'picsum.photos'],
   },
+  experimental: {
+    optimizeCss: true,
+    swcMinify: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.optimization.splitChunks.cacheGroups = {
+        framework: {
+          chunks: 'all',
+          name: 'website',
+          test: /(?<!node_modules.*)[\\/]node_modules[\\/](react|react-dom|@next|scheduler|prop-types|use-subscription)[\\/]/,
+          priority: 40,
+          enforce: true,
+        },
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
